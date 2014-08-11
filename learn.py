c5a1ed3c1ed9209608092e33220944322cc4bb24
phonnz <agonzalez@nyxtechnology.com>
2014-08-10 22:42:48 -0500

    Sets managing

--- learn.py
+++ learn.py
@@ -63,4 +63,42 @@ for key, value in ninjas.items():
 	if value == 'front':
 		print key
 
-	
+print 'Merge lists'
+owners = ['information', 'sells', 'contact']
+emails = ['info@nyxtechnology.com', 'ventas@nyxtechnology.com', 'contact@nyxtechnology.com']
+print owners
+print emails
+print 'Using zip() you can merge lists'
+print zip(owners, emails)
+print 'And you can merge and convert to dictionary'
+print dict(zip(owners, emails))
+
+print 'You can filter items on lists pretty easy'
+myList = [2, 5, 9 ,-3 , 0, -4, 2, 0]
+print myList
+filteredList = [n for n in myList if n > 0]
+print filteredList
+
+print 'Now filter a dictionary'
+diccionario = {'a':1, 'e':4, 'c':50, 'd':3, 'b':10, 'f':7}
+print diccionario
+filteredDictionary = [{k: v} for k, v in diccionario.items() if v%2 == 0]
+print filteredDictionary
+
+print 'Managing sets'
+first = [1, 3, 5, 7]
+second  = [5, 7, 9, 13]
+print 'A: ', first
+print 'B: ', second
+print 'Union'
+print set(first) | set(second)
+print 'Intersection'
+print set(first) & set(second)
+print 'A - B'
+print set(first) - set(second)
+print 'B - A'
+print set(second) - set(first)
+print 'Clean sets'
+third = [0, 0, 3, 4, 5, 7, 9, 2, 4, 7, 8, 3]
+print third
+print list(set(third))
