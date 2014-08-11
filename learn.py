5d630e28c7e752aa14f7147858fccac05640fe5d
phonnz <agonzalez@nyxtechnology.com>
2014-08-10 23:33:08 -0500

    functions

--- learn.py
+++ learn.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-print 'Hellow world!'
+print '*** Hellow world! ***'
 
 ##variables
 simpleString = 'hello world'
 simpleInteger = 1532
 
-print('know type of variable')
+print(' *** know type of variable *** ')
 print type(simpleString)
 
 print 'Define a list'
@@ -18,7 +18,7 @@ print myList
 
 print 'Concatenate strings: ', simpleString
 
-print 'Define dictionaries'
+print '*** Define dictionaries ***'
 diccionario = {'a':1, 'e':4, 'c':50, 'd':'d', 'b':10}
 print diccionario
 
@@ -29,7 +29,7 @@ print 'You can unpackage lists on variables'
 a, b, c, d, e, f = myList
 print a, b, c, d, e, f
 
-print 'Make loops on python is pretty easy'
+print ' *** Make loops on python is pretty easy *** '
 for l in myList:
 	print l
 
@@ -44,7 +44,7 @@ for l in diccionario.items():
 for key, value in diccionario.items():
 	print 'key: ', key, ' value: ', value
 
-print 'You can sort with sorted()'
+print '*** You can sort with sorted() *** '
 print sorted(myList)
 print 'Or you can use reverse for descending sort'
 print sorted(myList, reverse=True)
@@ -54,7 +54,7 @@ print sorted(diccionario.items())
 print 'But you can define the param to be sort'
 print sorted(diccionario.items(), key=lambda x: x[1])
 
-print 'You can filter what you use from dictionary'
+print ' *** You can filter what you use from dictionary *** '
 ninjas = {'hugo':'front', 'iorch':'front', 'mario':'backend', 'luis':'ios'}
 print ninjas
 print 'Selecting just who are front'
@@ -63,7 +63,7 @@ for key, value in ninjas.items():
 	if value == 'front':
 		print key
 
-print 'Merge lists'
+print ' *** Merge lists *** '
 owners = ['information', 'sells', 'contact']
 emails = ['info@nyxtechnology.com', 'ventas@nyxtechnology.com', 'contact@nyxtechnology.com']
 print owners
@@ -85,7 +85,7 @@ print diccionario
 filteredDictionary = [{k: v} for k, v in diccionario.items() if v%2 == 0]
 print filteredDictionary
 
-print 'Managing sets'
+print ' *** Managing sets *** '
 first = [1, 3, 5, 7]
 second  = [5, 7, 9, 13]
 print 'A: ', first
@@ -103,7 +103,7 @@ third = [0, 0, 3, 4, 5, 7, 9, 2, 4, 7, 8, 3]
 print third
 print list(set(third))
 
-print 'Working with slices'
+print ' *** Working with slices *** '
 text = '-This is the complete string'
 print text
 print 'Now just the first 10 characters'
@@ -118,7 +118,7 @@ print third
 print 'Jumping in the list in threes'
 print third[::3]
 
-print 'Delete elements'
+print ' *** Delete elements *** '
 print third
 print 'Delete the first element'
 del third[0]
@@ -131,3 +131,36 @@ print diccionario
 print 'Delete when key = c'
 del diccionario['c']
 print diccionario
+
+print '*** Regular expressions ***'
+print 'You need to import re'
+import re
+print 'Reordering date on a string with regular expressions'
+text = 'today is 1/10/1987'
+print text
+print re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\2-\1', text)
+print 'String format'
+text = '{} is a {}'
+print text.format('leopard', 'feline')
+text = '{animal} is a {family}'
+print text.format(family = 'mammal', animal = 'human')
+data = {'animal':'Elephant', 'family':'mammal'}
+print text.format(**data)
+
+print '*** Function ***'
+def hello():
+	print '- Hello this is a function'
+hello()
+
+print 'Now with params'
+def hello(name):
+	print '- Hello %s this is a function' % name
+hello('phonnz')
+
+def hello(*args, **kwargs):
+	print 'args ', args
+	print 'kwargs ', kwargs
+print 'Function with args'
+hello('python', 'learning')
+print 'Function with kwargs'
+hello(language='python', action='learning')
